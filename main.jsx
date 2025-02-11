4d06e310dddf375691c94e7e88808fa2bf1dee7b
gwdvb323 <igorvdmvc@gmail.com>
2025-02-11 22:39:38 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import VI_Coin from "./ViCoin";
-import "./styles.css";
+import VI_Coin from "./ViCoin"; // Проверить правильность пути
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
