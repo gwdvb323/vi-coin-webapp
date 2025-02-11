e6325a658e1c5458e7f88f82a58a66d8f3391f88
gwdvb323 <igorvdmvc@gmail.com>
2025-02-11 22:46:48 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import VI_Coin from "./ViCoin"; // Проверить правильность пути
+import VI_Coin from "./VI_Coin"; // <-- Измени на это
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <VI_Coin />
   </React.StrictMode>
 );
+
